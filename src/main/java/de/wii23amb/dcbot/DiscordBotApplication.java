64299e7749bfd36d49ed3677bc777e1c1fd81e31
Anton Bowe <mail@antonbowe.de>
2023-10-10 15:50:56 +0200

    initial commit

---
+++ src/main/java/de/wii23amb/dcbot/DiscordBotApplication.java
@@ -0,0 +1,13 @@
+package de.wii23amb.dcbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wii23amb/dcbot/DiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wii23amb.dcbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
